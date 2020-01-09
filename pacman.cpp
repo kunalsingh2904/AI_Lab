c1c370050d5c381cbb7f218f93bf6d6e67ee2ec3
adityajha1181998 <44377261+adityajha1181998@users.noreply.github.com>
2020-01-10 00:11:44 +0530

    Update pacman.cpp

--- pacman.cpp
+++ pacman.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 
-int brk=0;
-int nse=0;
+int nv=0;
 int plen=0;
+
 
 
 struct cell{
@@ -24,8 +24,44 @@ struct adj{
 
 
 struct graph{
+  int level = 0;
+  cell parent;
   int color=0;
 };
+
+
+
+void traceprint(vector<string> str, vector <vector<graph>>  &g, cell goal,cell root)
+{
+  cell tc;
+  tc = goal;
+  while(g[tc.x][tc.y].parent.x != root.x || g[tc.x][tc.y].parent.y != root.y )
+  {
+    str[tc.x][tc.y] = '0';
+
+    tc = g[tc.x][tc.y].parent;   plen++;
+  }   
+  str[tc.x][tc.y] = '0';
+  str[0][0]  = '0';  
+  str[root.x][root.y]  = '0'; plen = plen + 3;
+  
+  
+  
+  
+
+cout<<nv<<"\n"<<plen<<"\n";
+
+for(int i=0;i<=str.size()-1;i++ )
+{
+  cout<<str[i]<<"\n";
+}
+
+}
+
+
+
+
+
 
 
 
@@ -36,10 +72,12 @@ for(unsigned int k=0;k<=adjan[root.x][root.y].list.size()-1;k++  )
    {
     if(g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].color == 0)
     {
-      //cout<<"Zz ---  "<<adjan[root.x][root.y].list[k].x<<" "<<adjan[root.x][root.y].list[k].y<<"  "<<g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].color<<"\n";
       st.push(adjan[root.x][root.y].list[k]  );
       g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].color = 1;
-     // cout<<"Zz2 ---  "<<adjan[root.x][root.y].list[k].x<<" "<<adjan[root.x][root.y].list[k].y<<"  "<<g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].color<<"\n";
+          nv++;
+      g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].parent = root;
+      g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].level = g[ g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].parent.x  ][g[adjan[root.x][root.y].list[k].x][adjan[root.x][root.y].list[k].y].parent.y].level + 1;
+
 
     }
    }
@@ -57,35 +95,128 @@ int  goaltest(cell obs,cell goal)
     return -1;
   }
 }
-
-void DFS_s(vector <vector<adj>> &adjan, vector <vector<graph>>  &g, cell root, cell goal)
-{
-
-stack <cell> st;
-
-movegen(st,root,adjan,g);
-g[root.x][root.y].color = 1;
-cout<<"SSS "<<st.size()<<"\n";
-while( !st.empty() )
-{
-  if(goaltest(st.top(),goal) == 1)
+//hidden DFS_s 
+  void DFS_s(vector <vector<adj>> &adjan, vector <vector<graph>>  &g, cell root, cell goal)
   {
-    cout<<"Tis "<<st.top().x<<" "<<st.top().y<<"\n";
-    break;
-  }
-  else
-  {
-    // cout<<"That "<<st.top().x<<" "<<st.top().y<<"\n";
 
-    cell tc = st.top();
-    st.pop();
-    movegen( st ,tc, adjan,g);
-
-  }
+  stack <cell> st;
   
+  movegen(st,root,adjan,g);
+  
+  g[root.x][root.y].color = 1;  nv++;
+  g[root.x][root.y].parent = root;
+
+
+  while( !st.empty() )
+  {
+    if(goaltest(st.top(),goal) == 1)
+    {
+      
+      break;  
+
+    }
+    else
+    {
+      // cout<<"That "<<st.top().x<<" "<<st.top().y<<"\n";
+
+      cell tc = st.top();
+      st.pop();
+      //g[tc.x][tc.y].color = 1;
+      movegen( st ,tc, adjan,g);
+
+    }
+    
+  }
+
+  }
+
+
+
+
+
+//DFID
+  int DFS_dep(vector <vector<adj>> &adjan, vector <vector<graph>>  g, cell root, cell goal, int dep,vector<string> str)
+  {
+
+  stack <cell> st;
+  st.push(root);
+
+  if( dep > 0)
+    {
+      movegen(st,root,adjan,g);
+
+    }
+
+  g[root.x][root.y].color = 1;  nv++;
+  g[root.x][root.y].parent = root;
+
+  
+
+  while( !st.empty() )
+  {
+    if(goaltest(st.top(),goal) == 1 )
+    {
+      
+      traceprint(str, g, goal, root);
+
+      return 1;  
+
+    }
+    else
+    {
+
+      cell tc = st.top();
+      st.pop();
+
+      if( g[tc.x][tc.y].level < dep )
+      {
+        movegen( st ,tc, adjan,g);
+        
+      }
+
+    }
+    
+  }
+  return -1;
+
+  }
+
+  void DFid(vector <vector<adj>> &adjan, vector <vector<graph>>  &g, cell root, cell goal, vector<string> str  )
+  {
+    int depth = 0;
+    while(  DFS_dep(adjan, g, root, goal, depth, str) != 1 )
+    {
+      
+      depth++;
+    }
+
+  }
+
+
+
+
+
+void dispadj(vector <vector<adj>> &adjan, int n, int m)
+{
+//hidden display adj
+  for(int i=0;i<=n-1;i++ ) // display adj
+  {
+    for(int j=0;j<=m-1;j++ )
+
+      {
+        cout<<i<<" "<<j<<" : ";
+        if(adjan[i][j].list.size() != 0)
+        {
+          for(unsigned int k=0;k<=adjan[i][j].list.size()-1;k++  )
+          {
+            cout<<"|"<<adjan[i][j].list[k].x<<" "<<adjan[i][j].list[k].y<<"| ";
+          }
+        }
+        cout<<"\n";
+      }
+  }
 }
 
-}
 
 
 
@@ -95,50 +226,37 @@ while( !st.empty() )
 
 
 
-
-int main(int argc,char** argv){
+int main(int argc,char** argv)
+{
 
 vector<string> str;
 string tstr;
 int i=0;
-cout<<argv[1]<<"\n";
 ifstream infile(argv[1]);
 
 
 
 int typ;
 infile>>typ;
-cout<<typ<<"\n";
 
 infile.ignore();
 
-// getline(infile,tstr,'\n');
-//
-//
-// int lenstr=0;
-// while(tstr[lenstr] != '\0'){
-//   lenstr++;
-// }
-//
-//
-// str.push_back(tstr.substr(0,lenstr));
 
-// cout<<lenstr<<"\n";
 
 
 while(getline(infile,tstr,'\n') ){
-  cout<<strlen(tstr.c_str())<<"\n";
 
   str.push_back(tstr);
   i++;
 }
-for(int i=0;i<=str.size()-1;i++ )
-{
-  cout<<str[i]<<"\n";
-}
+// for(int i=0;i<=str.size()-1;i++ )    // disp grid
+// {
+//   cout<<str[i]<<"\n";
+// }
+
 int n =str.size();
 int m = strlen(str[0].c_str());
-cout<<"nxm "<<n<<" "<<m<<"\n";
+//cout<<"nxm "<<n<<" "<<m<<"\n";
 
 vector <vector<adj>> adjan(n,vector<adj>(m));
 
@@ -223,52 +341,9 @@ vector <vector<adj>> adjan(n,vector<adj>(m));
       {
         if(str[i][j] == ' ')
         {
-              if(  str[i][j-1] ==' ' ){
 
-                cell ct;
-                ct.x = i;
-                ct.y = j-1;
-
-                adjan[i][j].list.push_back(ct);
-
-                // ct.x = i;
-                // ct.y = j;
-                //
-                // adjan[i][j-1].list.push_back(ct);
-
-              }
-
-              if(  str[i][j+1] ==' ' ){
-
-                cell ct;
-                ct.x = i;
-                ct.y = j+1;
-
-                adjan[i][j].list.push_back(ct);
-
-                // ct.x = i;
-                // ct.y = j;
-                //
-                // adjan[i][j+1].list.push_back(ct);
-
-              }
-
-              if(  str[i-1][j] ==' ' ){
-
-                cell ct;
-                ct.x = i-1;
-                ct.y = j;
-
-                adjan[i][j].list.push_back(ct);
-
-                // ct.x = i;
-                // ct.y = j;
-                //
-                // adjan[i-1][j].list.push_back(ct);
-
-              }
-
-              if(  str[i+1][j] ==' ' ){
+               if(  str[i+1][j] ==' ' ) // UP
+               {
 
                 cell ct;
                 ct.x = i+1;
@@ -276,12 +351,53 @@ vector <vector<adj>> adjan(n,vector<adj>(m));
 
                 adjan[i][j].list.push_back(ct);
 
-                // ct.x = i;
-                // ct.y = j;
-                //
-                // adjan[i+1][j].list.push_back(ct);
 
               }
+
+
+               if(  str[i-1][j] ==' ' ) // DOWN
+               {
+
+                cell ct;
+                ct.x = i-1;
+                ct.y = j;
+
+                adjan[i][j].list.push_back(ct);
+
+
+              }
+
+
+
+              if(  str[i][j+1] ==' ' ) // RIGHT
+              {
+
+                cell ct;
+                ct.x = i;
+                ct.y = j+1;
+
+                adjan[i][j].list.push_back(ct);
+
+
+              }
+
+              if(  str[i][j-1] ==' ' ) // LEFT
+              {
+
+                cell ct;
+                ct.x = i;
+                ct.y = j-1;
+
+                adjan[i][j].list.push_back(ct);
+
+
+              }
+
+
+
+             
+
+
 
 
           }
@@ -329,39 +445,34 @@ vector <vector<adj>> adjan(n,vector<adj>(m));
 
   }
 
+
 vector <vector<graph>> g(n,vector<graph>(m)); // graph fixed index
 
-for(int i=0;i<=n-1;i++ ) // display adj
+
+dispadj(adjan,n,m);
+
+
+
+
+
+//cout<<"root "<<root.x<<" "<<root.y<<"\n";
+//cout<<"goal "<<goal.x<<" "<<goal.y<<"\n";
+
+
+if(typ == 1)
 {
-  for(int j=0;j<=m-1;j++ )
+  DFS_s(adjan,g,root,goal);
+  traceprint(str, g, goal, root);
 
-    {
-      cout<<i<<" "<<j<<" : ";
-      if(adjan[i][j].list.size() != 0)
-      {
-        for(unsigned int k=0;k<=adjan[i][j].list.size()-1;k++  )
-        {
-          cout<<"|"<<adjan[i][j].list[k].x<<" "<<adjan[i][j].list[k].y<<"| ";
-        }
-      }
-      cout<<"\n";
-    }
 }
-
-
-
-
-
-
-
-cout<<"root "<<root.x<<" "<<root.y<<"\n";
-cout<<"goal "<<goal.x<<" "<<goal.y<<"\n";
-cout<<adjan[root.x][root.y].list[0].x<<" "<<adjan[root.x][root.y].list[0].y<<"||\n";
-
-//DFS(adjan,g,root);
-
-DFS_s(adjan, g, root, goal);
-
+else if(typ == 2)
+{
+  DFid(adjan, g, root, goal, str);
+}
+else
+{
+cout<<typ<<" search type not clear";
+}
 
 
 
